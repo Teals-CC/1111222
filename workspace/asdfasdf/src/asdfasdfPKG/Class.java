31e09f2a62bf3caa8e79742807efcd05973846ac
apcstest <apcstest@users.noreply.github.com>
2014-10-19 14:34:48 -0700

    Update Class.java

--- workspace/asdfasdf/src/asdfasdfPKG/Class.java
+++ workspace/asdfasdf/src/asdfasdfPKG/Class.java
@@ -5,13 +5,13 @@ public class Class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("asdfad");
+		System.out.println("asdf");
+		System.out.println("asdf");
 		System.out.println("asdfad");
-		System.out.println("asdfad");
-		System.out.println("asdfad");
-		System.out.println("asdfad");
-		System.out.println("asdfad");
-		System.out.println("asdfad");
-		System.out.println("asdfad");
+		System.out.println("asdxcvbfad");
+		System.out.println("vbx");
+		System.out.println("xcvb");
+		System.out.println("asdxvbfad");
 		
 	}
 
